7a15e5313694a36bae8c343d2d544b3240982b64
nickyy <lima.nascimento.nicole@escola.pr.gov.br>
2026-08-14 09:24:03 -0300

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+const_caixaPrincipal = document.querySelector (".caixa-Principal");
+const_caixaPerguntas = document.querySelector (".caixa-Perguntas");
+const_caixaAlternativas = document.querySelector (".caixa-Alternativas");
+const_caixaResultado= document.querySelector (".caixa-Resultado");
+const_textoResultado= document.querySelector (".texto-Resultado");
+
+const_perguntas = [
+    
+    "Perguntas1",
+    
+    "Perguntas2"
+
+]
+
